9570d28f18203993a6e13ae5bd20559f99ff215b
zhi_cheng233 <mczhicheng@outlook.com>
2025-05-31 22:20:37 +0800

    添加项目文件。

---
+++ DG-LABMai/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(DG_LABMai.Core), "DG-LABMai", "1.0.0", "zhi_cheng233", null)]
+[assembly: MelonGame("sega-interactive", "Sinmai")]
+
+namespace DG_LABMai
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
